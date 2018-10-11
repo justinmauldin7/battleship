fdce2489aace0b2da1cf154abdfbc6756e2ff896
Justin Mauldin <justin.mauldin7@gmail.com>
2018-10-10 19:21:29 -0600

    Adds changes made toquit q exit esc :wq

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,20 @@
+require './lib/spaces'
+
 class Board
-  
+  attr_accessor :template
+  def initialize
+  end
+
+
+def template
+    {
+      "a1" => " ", "a2" => " ", "a3" => " ", "a4" => " ",
+      "b1" => " ", "b2" => " ", "b3" => " ", "b4" => " ",
+      "c1" => " ", "c2" => " ", "c3" => " ", "c4" => " ",
+      "d1" => " ", "d2" => " ", "d3" => " ", "d4" => " "
+  }
+end
+
+
+
 end
