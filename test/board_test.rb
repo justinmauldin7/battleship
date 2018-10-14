3b8ef11d5728ddbe28213b571acd2ef232a2316a
Justin Mauldin <justin.mauldin7@gmail.com>
2018-10-14 12:57:29 -0600

    Adds changes to board_test file to get it to work & adds test for spaces existing.

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,4 +1,4 @@
-require_relative '/test/test_helper'
+require './test/test_helper'
 require './lib/board'
 
 class BoardTest < Minitest::Test
@@ -8,15 +8,8 @@ class BoardTest < Minitest::Test
     assert_instance_of Board, board
   end
 
-  def test_board_has_a_template
+  def test_a_space_on_the_template_exists
     board = Board.new
-    expected = ({
-    a1: =>  ,a2: =>  ,a3: =>  ,a4: =>  ,
-    b1: =>  ,b2: =>  ,b3: =>  ,b4: =>  ,
-    c1: =>  ,c2: =>  ,c3: =>  ,c4: =>  ,
-    d1: =>  ,d2: =>  ,d3: =>  ,d4: =>
-  })
-    assert_equal expected, board.template
-
+    assert_instance_of Space, board.template[:a1]
   end
 end
