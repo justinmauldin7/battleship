194e1aeefc587fbeb5ff969ffbed31723b3a8aac
Justin Mauldin <justin.mauldin7@gmail.com>
2018-10-15 12:57:24 -0600

    starts adding  tests for board & spaces classes.

--- test/board_test.rb
+++ test/board_test.rb
@@ -11,5 +11,49 @@ class BoardTest < Minitest::Test
   def test_a_space_on_the_template_exists
     board = Board.new
     assert_instance_of Space, board.template[:a1]
+    assert_instance_of Space, board.template[:a2]
+    assert_instance_of Space, board.template[:a3]
+    assert_instance_of Space, board.template[:a4]
+    assert_instance_of Space, board.template[:b1]
+    assert_instance_of Space, board.template[:b2]
+    assert_instance_of Space, board.template[:b3]
+    assert_instance_of Space, board.template[:b4]
+    assert_instance_of Space, board.template[:c1]
+    assert_instance_of Space, board.template[:c2]
+    assert_instance_of Space, board.template[:c3]
+    assert_instance_of Space, board.template[:c4]
+    assert_instance_of Space, board.template[:d1]
+    assert_instance_of Space, board.template[:d2]
+    assert_instance_of Space, board.template[:d3]
+    assert_instance_of Space, board.template[:d4]
   end
+
+  def test_print_the_ships_method
+    skip
+    board = Board.new
+    expected =
+      puts "==================="
+      puts ". 1 2 3 4"
+      puts "A #{@template[:a1].state} #{@template[:a2].state} #{@template[:a3].state} #{@template[:a4].state} "
+      puts "B #{@template[:b1].state} #{@template[:b2].state} #{@template[:b3].state} #{@template[:b4].state} "
+      puts "C #{@template[:c1].state} #{@template[:c2].state} #{@template[:c3].state} #{@template[:c4].state} "
+      puts "D #{@template[:d1].state} #{@template[:d2].state} #{@template[:d3].state} #{@template[:d4].state} "
+      puts "==================="
+    assert_equal expected, board.print_the_ships
+  end
+
+  def test_print_the_grid_method
+    skip
+    board = Board.new
+    expected =
+      puts "==================="
+      puts ". 1 2 3 4"
+      puts "A #{@template[:a1].shot_status} #{@template[:a2].shot_status} #{@template[:a3].shot_status} #{@template[:a4].shot_status} "
+      puts "B #{@template[:b1].shot_status} #{@template[:b2].shot_status} #{@template[:b3].shot_status} #{@template[:b4].shot_status} "
+      puts "C #{@template[:c1].shot_status} #{@template[:c2].shot_status} #{@template[:c3].shot_status} #{@template[:c4].shot_status} "
+      puts "D #{@template[:d1].shot_status} #{@template[:d2].shot_status} #{@template[:d3].shot_status} #{@template[:d4].shot_status} "
+      puts "==================="
+      assert_equal expected, board.print_the_grid
+  end
+
 end
--- test/spaces_test.rb
+++ test/spaces_test.rb
@@ -3,22 +3,19 @@ require './lib/spaces'
 
 class SpaceTest < Minitest::Test
 
-def setup
-  @space_a1 = Space.new({"a" => 1})
-  @space_b3 = Space.new({"b" => 3})
-  @space_c4 = Space.new({"c" => 4})
-  @space_d2 = Space.new({"d" => 2})
-end
 
-  def test_some_of_the_spaces_exist
-    assert_instance_of Space, @space_a1
-    assert_instance_of Space, @space_b3
-    assert_instance_of Space, @space_c4
-    assert_instance_of Space, @space_d2
+  def test_space_class_exists
+    space = Space.new
+    assert_instance_of Space, space
   end
 
-  def test_it_adds_a_shot
-      assert_equal 1, @space_a1.add_shot_status
+  def test_shot_status_defaults_to_nil
+    skip
   end
+
+  def test_state_defaults_to_empty
+    skip
+  end
+
 
 end
