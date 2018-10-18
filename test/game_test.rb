a7d8618b45f6704e2ca90dd9c6ba230dac5af1dc
Jennica Stiehl <jennica.stiehl@gmail.com>
2018-10-17 19:48:16 -0600

    Adds tests for the game class.

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,20 +1,33 @@
-require_relative '/test/test_helper'
+require './test/test_helper'
 require './lib/game'
 
 class GameTest < Minitest::Test
   def test_the_game_exists
-    skip
     game = Game.new
     assert_instance_of Game, game
   end
 
-  def test_if_we_can_fill_a_space
+  def test_if_end_game_sequence
+    skip
     game = Game.new
-    assert_equal "F", game.user_input_for_coordinates
+    human_player = Player.new
+    human_player.player_hits = 5
+    assert_equal "Thanks for playing BATTLESHIP!", game.end_game_sequence
   end
 
-  def test_a
-    skip
+  def test_a_computer_can_place_a_ship
+    computer_player = Player.new
+    human_board = Board.new
+    @template =
+      {
+        a1: Space.new, a2: Space.new, a3: Space.new, a4: Space.new,
+        b1: Space.new, b2: Space.new, b3: Space.new, b4: Space.new,
+        c1: Space.new, c2: Space.new, c3: Space.new, c4: Space.new,
+        d1: Space.new, d2: Space.new, d3: Space.new, d4: Space.new,
+
+    }
+    computer_player.computer_takes_a_shot(human_board)
+    assert_equal "E", @template[:a1].state
   end
 
 end
